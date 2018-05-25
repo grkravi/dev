52de71d57f14736495b3b23d8484b334fb44a09c
grkravi <grkravi@hotmail.com>
2018-05-25 08:47:24 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Sangrav Members</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello Sangrav Members</h1>
+                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <div align="center"><img src="https://s3.amazonaws.com/acloudguru-opsworkslab-donotdelete/logo-on-black-800.png"></div>
+                <p>Your application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
